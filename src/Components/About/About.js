d73ca28113d352302d44fb60905d58b3d18e75cb
juhayerayaz <aiazj97@gmail.com>
2022-05-09 23:33:26 +0600

    about update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='container'>
-            <h1 className='text-center'>Razer House</h1>
-            <p className='text-center'>We are a Gaming warehouse company where we sell many products related to games. Our hero product is our chairs. Besides we have many other things. We have been doing business for over 10 years accross the globe.</p>
+        <div className='container mt-5 mb-5'>
+            <h1 className='text-center fw-bold'>Razer House</h1>
+            <p className='text-center fw-light'>We are a Gaming warehouse company where we sell many products related to games. Our hero product is our chairs. <br />Besides we have many other things. We have been doing business for over 10 years accross the globe.</p>
         </div>
     );
 };
