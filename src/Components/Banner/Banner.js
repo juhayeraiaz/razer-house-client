0dbea71b0769d560a84b409743fccda5ddba053d
juhayerayaz <aiazj97@gmail.com>
2022-10-24 20:45:19 +0600

    navbar banner review map fixed

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,15 +5,15 @@ import { Carousel } from 'react-bootstrap';
 
 export default function App() {
     return (
-        <div className='mt-5 mb-5'>
+        <div className='mb-5'>
             <Carousel>
-                <Carousel.Item className='active first-carousel-item'>
+                <Carousel.Item className='first-carousel-item'>
                     <div className='carousel-text px-4'>
                         <p className='text-white-50'>Welcome to</p>
                         <h1 className='carousel-title fw-bolder text-white'>Razer House</h1>
                         <p className='text-white-50'>We provide the best quality gaming chairs and accessories accross the world. <br /> Our products come in many designs and category.</p>
-                        <Link to='/items'><button className='btn btn-outline-light rounded-pill  fw-bold px-4 py-2'>Our Products</button></Link>
-                        <Link to='/about'><button className='btn btn-outline-dark text-white rounded-pill fw-bold  ms-3 px-4 py-2'>About us</button></Link>
+                        <Link to='/items'><button className='btn btn-light rounded-pill  fw-bold px-4 py-2'>Our Products</button></Link>
+                        <Link to='/about'><button className='btn btn-dark text-white rounded-pill fw-bold  ms-3 px-4 py-2'>About us</button></Link>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item className='second-carousel-item'>
@@ -21,7 +21,7 @@ export default function App() {
                         <p className='text-white-50'>Storage and capacity</p>
                         <h1 className='carousel-title fw-bolder text-white'>Mass Storage</h1>
                         <p className='text-white-50'>As a world-wide organization we need mass storage to supply everywhere in the world</p>
-                        <Link to='/items'><button className='btn btn-outline-light rounded-pill  fw-bold px-4 py-2'>Our Products</button></Link>
+                        <Link to='/items'><button className='btn btn-light rounded-pill  fw-bold px-4 py-2'>Our Products</button></Link>
                     </div>
                 </Carousel.Item>
                 <Carousel.Item className='third-carousel-item'>
@@ -30,7 +30,7 @@ export default function App() {
                         <h1 className='carousel-title-third fw-bolder text-white'>GROUND OR AIR,</h1>
                         <h1 className='carousel-title-third fw-bolder text-white'> THERE IS NO STOPPING FOR US</h1>
                         <p className='text-white-50'>Starting from loading to unloading and maintaining the highest standards <br /> in terms of safety while in transit, we take nothing to chance.</p>
-                        <Link to='/items'><button className='btn btn-outline-light rounded-pill  fw-bold px-4 py-2'>Our Products</button></Link>
+                        <Link to='/items'><button className='btn btn-light rounded-pill  fw-bold px-4 py-2'>Our Products</button></Link>
                     </div>
                 </Carousel.Item>
             </Carousel>
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { signOut } from 'firebase/auth';
+import './Header.css'
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import logo from '../../../images/logo.png'
@@ -7,13 +8,23 @@ import auth from '../../../firebase.init';
 import { Link } from 'react-router-dom';
 const Header = () => {
     const [user] = useAuthState(auth);
+    const [offset, setOffset] = useState(0);
 
     const handleSignOut = () => {
         signOut(auth);
     }
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset);
+        // clean up code
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky='top' className='p-3'>
+            <Navbar collapseOnSelect expand="lg" variant={offset <= 400 ? 'dark' : 'light'} bg={offset <= 400 ? '' : 'light'} sticky='top' className='headr'>
                 <Container>
                     <Navbar.Brand as={Link} to='home'><img
                         src={logo}
@@ -22,24 +33,22 @@ const Header = () => {
                         alt="React Bootstrap logo"
                     /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Navbar.Collapse id="responsive-navbar-nav" className='nav-collapse'>
                         <Nav className="me-auto">
                         </Nav>
                         <Nav className='mt-4 text-center mt-md-0'>
-                            <Nav.Link as={Link} to='items'>Items</Nav.Link>
-                            <Nav.Link as={Link} to='about'>About</Nav.Link>
+                            <Nav.Link as={Link} className='mx-0 mx-md-2' style={offset <= 400 ? { color: 'white' } : { color: 'black' }} to='items'>Items</Nav.Link>
+                            <Nav.Link as={Link} className='mx-0 mx-md-2' style={offset <= 400 ? { color: 'white' } : { color: 'black' }} to='about'>About</Nav.Link>
                             {
                                 user ?
                                     <>
-                                        <Nav.Link as={Link} to='manage'>Manage</Nav.Link>
-                                        <Nav.Link as={Link} to='add'>Add item</Nav.Link>
+                                        <Nav.Link as={Link} className='mx-0 mx-md-2' style={offset <= 400 ? { color: 'white' } : { color: 'black' }} to='manage'>Manage</Nav.Link>
+                                        <Nav.Link as={Link} className='mx-0 mx-md-2' style={offset <= 400 ? { color: 'white' } : { color: 'black' }} to='add'>Add item</Nav.Link>
                                         <img className='rounded-circle' src={user.photoURL} width='35px' alt="" />
-                                        <button className='btn btn-link text-dark text-decoration-none' onClick={handleSignOut}>Log out</button>
+                                        <Nav.Link className='mx-0 mx-md-2' style={offset <= 400 ? { color: 'white' } : { color: 'black' }} onClick={handleSignOut}>Log out</Nav.Link>
                                     </>
-
-
                                     :
-                                    <Nav.Link as={Link} to="login">
+                                    <Nav.Link as={Link} className='mx-0 mx-md-2' style={offset <= 400 ? { color: 'white' } : { color: 'black' }} to="login">
                                         Login
                                     </Nav.Link>}
                         </Nav>
